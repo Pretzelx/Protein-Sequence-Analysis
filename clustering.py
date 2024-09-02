ae33306b6fa9d33df5e1ec936f94a01b326cb221
Prerna Duhan <prerna.duhan@students.fhnw.ch>
2024-09-02 20:08:16 +0200

    file added

--- clustering.py
+++ clustering.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans #KMeans is imported from sklearn.cluster, which is a machine learning algorithm used for clustering data.
+from sklearn.decomposition import PCA
 import numpy as np
 
 # Define the set of amino acids
@@ -9,6 +10,11 @@ def encode_sequence(sequence, amino_acids=amino_acids):
 
 def perform_clustering(sequences, n_clusters=2):
     encoded_sequences = np.array([encode_sequence(seq) for seq in sequences])
+    pca = PCA(n_components=2)
+    reduced_data = pca.fit_transform(encoded_sequences)
+    
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-    kmeans.fit(encoded_sequences)
-    return kmeans.labels_
+    kmeans.fit(reduced_data)
+    
+    return kmeans.labels_, reduced_data
+
--- visualisation.py
+++ visualisation.py
@@ -3,13 +3,13 @@ from sklearn.decomposition import PCA
 import numpy as np 
 from clustering import encode_sequence
 
-# Define the set of amino acids
-amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
 
-def plot_clusters(sequences, labels, amino_acids=amino_acids):
-    encoded_sequences = np.array([encode_sequence(seq) for seq in sequences])
-    pca = PCA(n_components=2)
-    reduced_data = pca.fit_transform(encoded_sequences)
-    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=labels)
-    plt.title('Clustering of Amino Acid Sequences')
+def plot_clusters(sequences, labels, reduced_data):
+    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=labels, cmap='viridis')
+    for i, sequence in enumerate(sequences):
+        plt.annotate(sequence, (reduced_data[i, 0], reduced_data[i, 1]))
+    
+    plt.title('PCA of Amino Acid Sequences')
+    plt.xlabel('PCA Component 1')
+    plt.ylabel('PCA Component 2')
     plt.show()
